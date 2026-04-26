541667dd5a858d1b27f619cfbace2013f766bd8f
Abdullah2265Habib <abdullah2006habib@gmail.com>
2026-04-26 17:36:54 +0500

    write function inside js for signup

--- frontend/assets/js/signup.js
+++ frontend/assets/js/signup.js
@@ -8,27 +8,40 @@ const submitBtn = document.querySelector('.form-container .form-row input[type="
 
 
 
-function signup() {
+// Toggle password visibility
+document.getElementById('signup-eye').addEventListener('click', function () {
+    const passInput = document.getElementById('password');
+    const icon = this.querySelector('i');
 
+    if (passInput.type === 'password') {
+        passInput.type = 'text';
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+    } else {
+        passInput.type = 'password';
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+    }
+});
+
+// Signup function (preserved from original)
+function signup() {
     let form = new FormData();
 
     form.append("name", document.getElementById("name").value);
     form.append("email", document.getElementById("email").value);
     form.append("password", document.getElementById("password").value);
 
-    fetch("../../backend/signup.php", {
+    fetch("http://localhost/Student-Productivity-Hub/backend/signup.php", {
         method: "POST",
         body: form
     })
         .then(res => res.text())
         .then(msg => {
-
             if (msg == "success") {
                 window.location = "../../index.html";
             } else {
-                alert("Invalid Signup");
+                alert("Email already registered");
             }
-
         });
-
 }
